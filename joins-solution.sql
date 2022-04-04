a28425511f07ab10a392487b61184d9eb77bf5ea
Brandon McCarty <brandon.m.mccarty12@gmail.com>
2022-04-04 17:04:03 -0500

    add base questions finished

--- joins-solution.sql
+++ joins-solution.sql
@@ -15,3 +15,27 @@ JOIN "warehouse_product" ON "warehouse".id = "warehouse_product".warehouse_id
 JOIN "products" ON "warehouse_product".product_id = "products".id
 WHERE "products".id = 5
 ;
+
+-- 4. Which warehouses have diet pepsi?
+SELECT "products".description, "warehouse".warehouse from "warehouse"
+JOIN "warehouse_product" ON "warehouse".id = "warehouse_product".warehouse_id
+JOIN "products" ON "warehouse_product".product_id = "products".id
+WHERE "products".id = 6
+;
+
+-- 5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
+SELECT ("customers".first_name, "customers".last_name) AS "customer", COUNT("orders") FROM "customers"
+JOIN "addresses" ON "customers".id = "addresses".customer_id
+JOIN "orders" ON "addresses".id = "orders".address_id
+GROUP BY "customer";
+
+-- 6. How many customers do we have?
+SELECT COUNT("customers".id) FROM "customers";
+
+-- 7. How many products do we carry?
+SELECT COUNT("products".id) FROM "products";
+
+-- 8. What is the total available on-hand quantity of diet pepsi?
+SELECT SUM("warehouse_product".on_hand) FROM "warehouse_product"
+JOIN "products" ON "warehouse_product".product_id = "products".id
+WHERE "products".id = 6;
